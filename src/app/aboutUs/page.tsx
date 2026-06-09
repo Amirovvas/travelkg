bebc1180ef84a2ac6fb73ab0e34ab6c9508b61e1
Amirovvas <amirova.saida06@gmail.com>
2026-06-09 16:24:35 +0600

    home

---
+++ src/app/aboutUs/page.tsx
@@ -0,0 +1,10 @@
+import AboutUs from '@/components/pages/aboutUs/AboutUs';
+import React from 'react';
+
+const page = () => {
+    return (
+      <AboutUs/>
+    );
+};
+
+export default page;
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import scss from "./header.module.scss";
+import { FaUser } from "react-icons/fa";
+import Link from "next/link";
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className={scss.container}>
+      <div className="container">
+        <div className={scss.mainContainer}>
+          <div className={scss.logo}>
+            <img src="/travelLogo.png" alt="logo" />
+            <h2>TravelKG</h2>
+          </div>
+          <nav>
+            <Link href={"/"}>Областы</Link>
+            <Link href={"/"}>Туры</Link>
+            <Link href={"/aboutUs"}>О нас</Link>
+            <Link href={"/"}>Избранные</Link>
+          </nav>
+          <span>
+            <FaUser />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
